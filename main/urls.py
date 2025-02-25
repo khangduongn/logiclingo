44b38d168fd7d8ab19c5afc0403981f17ce50d8c
Khang Duong <knd57@drexel.edu>
2025-02-25 05:23:02 +0000

    Added users

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('classroom/<int:id>/', views.classroom, name='classroom'),
+    path('create_student/', views.create_student, name='create_student'),
+    path('create_instructor/', views.create_instructor, name='create_instructor'),
+    path('student/<int:id>/', views.student, name='student'),
+    path('instructor/<int:id>/', views.instructor, name='instructor'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from .forms import ClassroomForm
-from .models import Classroom
+from .forms import ClassroomForm, StudentForm, InstructorForm
+from .models import Classroom, Student, Instructor, User
+from django.http import Http404
 
 def index(request):
 
@@ -19,3 +20,53 @@ def index(request):
 def classroom(request, id):
     classroom = get_object_or_404(Classroom, classroomID=id)
     return render(request, 'classroom.html', {'classroom': classroom})
+
+def student(request, id):
+    student = get_object_or_404(Student, userID=id)
+    return render(request, 'student.html', {'student': student})
+
+def instructor(request, id):
+    instructor = get_object_or_404(Instructor, userID=id)
+    return render(request, 'instructor.html', {'instructor': instructor})
+
+def create_student(request):
+
+    createStudentForm = StudentForm()
+
+    if request.method == 'POST':
+        createStudentForm = StudentForm(request.POST)
+        if createStudentForm.is_valid():
+            newStudent = createStudentForm.save()
+            return redirect('profile', id=newStudent.userID)
+
+    return render(request, 'create_student.html', {'form': createStudentForm})
+
+def create_instructor(request):
+
+    createInstructorForm = InstructorForm()
+
+    if request.method == 'POST':
+        createInstructorForm = InstructorForm(request.POST)
+        if createInstructorForm.is_valid():
+            newInstructor = createInstructorForm.save()
+            return redirect('profile', id=newInstructor.userID)
+
+    return render(request, 'create_instructor.html', {'form': createInstructorForm})
+
+def login(request):
+    loginForm = LoginForm()
+    if request.method == 'POST':
+        loginForm = LoginForm(request.POST)
+        if loginForm.is_valid():
+            username = loginForm.cleaned_data['username']
+            password = loginForm.cleaned_data['password']
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('student') 
+            else:
+                loginForm.add_error(None, "Invalid username or password")
+
+    
+    return render(request, 'login.html', {'form': loginForm})
