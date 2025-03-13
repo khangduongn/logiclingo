3b202fbd65eca09d67c838cdeb2dc5d190ec1213
PCookie44 <petertcooke7@gmail.com>
2025-03-13 21:33:51 +0000

    Added model to import

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Classroom, Student, Instructor, User
+from .models import Classroom, Student, Instructor, User, Topic
 
 # Register your models here.
 admin.site.register(Classroom)
