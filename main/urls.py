80f3004592102fe94ce78f098fd2cf7520d5d399
PCookie44 <petertcooke7@gmail.com>
2025-03-13 22:07:41 +0000

    added html to urls

--- main/urls.py
+++ main/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('classroom/<int:id>/settings', views.classroom_settings, name='classroom_settings'),
     path('confirm-join/', views.confirm_join_classroom, name='confirm_join_classroom'),
     path('classroom/<int:classroomID>/create_topic/', views.create_topic, name='create_topic'),
+    path('classroom/<int:classroomID>/topic/<int:topicID>/', views.topic, name='topic'),
 ]
